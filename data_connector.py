6935c6aaa9982bd591ad5d139d92a2dc6b4b9167
John <john@eaxmple.com>
2024-06-01 22:25:07 +0200

    linked blob storage

---
+++ data_connector.py
@@ -0,0 +1,55 @@
+from azure.storage.blob import BlobServiceClient, BlobClient
+import pandas as pd
+from io import StringIO
+import io  # Import the io module
+from tqdm import tqdm
+import functools
+import constants
+
+# Replace with your actual connection string
+connection_string = 'DefaultEndpointsProtocol=https;AccountName=experimentstorage234;AccountKey=za0T9NgDLAP2vl4d1JSpRSUcyuATvcrj1ekoZ4Jh7dr4QfHeSN8YjW6e+cRAMiSiAXfTpJWfbK6j+AStcCPS9A==;EndpointSuffix=core.windows.net'
+container_name = 'data-swollen'
+master_file = constants.csv_file_path
+
+# Function to download blob with progress
+@functools.cache
+def download_blob_with_progress(blob_client, total_size):
+    stream = io.BytesIO()
+    chunk_size = 1024 * 1024  # 1MB
+    bytes_downloaded = 0
+
+    with tqdm(total=total_size, unit='B', unit_scale=True, desc='Downloading') as pbar:
+        downloader = blob_client.download_blob()
+        for chunk in downloader.chunks():
+            stream.write(chunk)
+            bytes_downloaded += len(chunk)
+            pbar.update(len(chunk))
+
+    stream.seek(0)
+    return stream
+
+def get_data():
+    try:
+        # Initialize the BlobServiceClient
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+        # Initialize the BlobClient
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=master_file)
+
+        # Get blob size
+        blob_properties = blob_client.get_blob_properties()
+        total_size = blob_properties.size
+
+        # Download the blob with progress
+        blob_data = download_blob_with_progress(blob_client, total_size).getvalue()
+
+        # Convert the blob's content to a StringIO object
+        data = StringIO(blob_data.decode('utf-8'))
+
+        # Read the data into a pandas DataFrame
+        master_data = pd.read_csv(data, on_bad_lines='warn', encoding='utf-8')
+
+        return master_data
+    except Exception as data_ex:
+        print(f'Ooops there was issue in get_data {data_ex}')
+        return None
